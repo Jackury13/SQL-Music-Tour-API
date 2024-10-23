77e02cdb360818845b3fe5ea6cd42cdcab89d7ac
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-10-23 02:44:04 -0700

    into to sequalize recommit

--- server.js
+++ server.js
@@ -10,16 +10,15 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-//sequelize connection
 // SEQUELIZE CONNECTION
-//const sequelize = new Sequelize(process.env.PG_URI)
+const sequelize = new Sequelize(process.env.PG_URI)
 
-//try {
-  //  sequelize.authenticate() 
-   // console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-//} catch(err) {
-   // console.log(`Unable to connect to PG: ${err}`) 
-//}
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
 
 
 // ROOT
