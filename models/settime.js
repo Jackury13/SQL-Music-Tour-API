3940a179f312a15268a3b63ac12afbd0d3bdaf15
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2024-10-31 05:22:51 -0700

    sequelize associations

--- models/settime.js
+++ models/settime.js
@@ -3,15 +3,17 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class SetTime extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  class Stage extends Model {
+    static associate({ Event, StageEvent }) {
+      // events
+      Stage.belongsToMany(Event, {
+        foreignKey: "stage_id",
+        as: "events",
+        through: StageEvent
+      })
     }
+  }
+  
   }
   SetTime.init({
     set_time_id: {
